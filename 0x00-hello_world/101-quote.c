f1982648b9627d349a0a069d8b48bb76101755d7
Magombedze <mhizhawetswanda@gmail.com>
2022-10-13 13:47:59 -0700

    101-quote correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
+/**
+   * Description: main - Prints out a string to stdout.
+    * Return: 0 if success.
+*/
+
+
 int main(void)
 {
 write(1, "and that piece of art is useful"
