c522ba06802c05d9c2c0dec32574816f2387cc26
Magombedze <mhizhawetswanda@gmail.com>
2022-10-13 11:46:03 -0700

    hello_world#

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-Entry
+*return :always 0
+   */
+
+int main(void)
+{
+	puts("\"Programming is like building a mult
+ilingual puzzle);
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("size of char:%i bytes\n", sizeof(char));
+	printf("size of int:%i bytes\n", sizeof(int));
+	printf("size of long int:%i bytes\n", sizeof(long int));
+	printf("size of long long int:%i bytes\n", sizeof(long long int));
+	printf("size of float:%i bytes\n", sizeof(float));
+return (0);
+
+
+
+}
+
