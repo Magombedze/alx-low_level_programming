fa885ce40dd64dd35b13fdf555bb4746188c52ac
Magombedze <mhizhawetswanda@gmail.com>
2022-10-15 03:31:04 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,13 +25,13 @@ if (lastn > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 
 }
+else if (lastn == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastn, n, lastn);
+}
 else if (lastn < 6 && lastn != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
-}
-else if (lastn == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, lastn);
 }
 
 
