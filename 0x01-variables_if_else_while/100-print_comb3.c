e8858f210462ef71286be00ab12fb2c46dd475d3
Magombedze <mhizhawetswanda@gmail.com>
2022-10-16 01:48:14 -0700

    betty 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+   * main - program that prints the numbers from 00 to 99
+    * Numbers must be separated by ,followed by a space
+     * Numbers should be printed in ascending order with 2 digits
+      * You can only use the putchar function
+      * Return: 0
+ */
+
+
+
+
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i > 48)
+{
+putchar(i);
+}
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
