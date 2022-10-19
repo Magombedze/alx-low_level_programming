d1b4cc76305de373f7cecb97a55ec247bb491d12
Magombedze <mhizhawetswanda@gmail.com>
2022-10-18 17:22:20 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include<main.h>
+
+/**
+   *  print_alphabet-a function that prints the alphabet,
+   *in lowercase, followed by a new line
+    *
+     * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+
+char i;
+i = 'a';
+while (i <= 'z')
+{
+_putchar('i');
+i++;
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include<main.h>
+
+/**
+* print_alphabet_x10 - function that prints 10 times the alphabet,
+    * in lowercase
+     * Return: nothing.
+      */
+
+
+
+void print_alphabet_x10(void)
+{
+int j;
+char i;
+for (j = 0; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+
+_putchar('i');
+}
+_putchar('\n');
+}
+
+}
+
