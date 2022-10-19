e0226a63df9617ec5b8f99db4914006b89bffede
Magombedze <mhizhawetswanda@gmail.com>
2022-10-18 17:43:05 -0700

    initial commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,18 +18,18 @@ if (n > 0)
 
 {
 
-_putchar(43);
+_putchar('+');
 return (1);
 
 }
 
-else if (n < 0)
+else if (n == 0)
 
 {
 
-_putchar(45);
+_putchar('0');
 
-return (-1);
+return (0);
 
 }
 
@@ -37,8 +37,8 @@ else
 
 {
 
-_putchar(48)
-return (0);
+_putchar('-')
+return (-1);
 }
 
 }
