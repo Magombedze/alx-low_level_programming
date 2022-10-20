b704d56b6da47dced3898565cda549572a28a375
Magombedze <mhizhawetswanda@gmail.com>
2022-10-20 14:52:42 -0700

    initial commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,16 +5,15 @@
 
 
 /**
-*main :a function that multiplies two integers
-   *
-     * Return: Always sum
-*/
+   *  mul - a function that multiplies two integers
+    * @a: first input
+     * @b: second input
+     * Return: multiplication result of the 2 inputs
+ */
 
 int mul(int a, int b)
 {
-int a;
-int b;
-int sum;
-sum = a * b;
-return sum;
+
+return (a * b);
+
 }
