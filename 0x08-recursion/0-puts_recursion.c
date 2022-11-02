363a11d78d750e3bdb2364488324d1f191014870
Magombedze <mhizhawetswanda@gmail.com>
2022-11-02 13:25:28 -0700

    initial commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,29 +1,35 @@
-#include "main.h"
-
-
+#include "holberton.h"
 
 /**
- * _puts_recursion - Entry Point
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
  */
 
 void _puts_recursion(char *s)
 
 {
 
-if (*s == '\0')
-
-_putchar('\n');
-
-else
+if (*s)
 
 {
 
-putchar(*s);
+_putchar(*s);
 
 _puts_recursion(s + 1);
 
 }
 
+else
+
+{
+
+_putchar('\n');
+
 }
+
+
+
+}
+
+
