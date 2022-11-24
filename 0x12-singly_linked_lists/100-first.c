fff398a8ec51b55020db935a7bb07b7636d81044
Magombedze <mhizhawetswanda@gmail.com>
2022-11-24 09:00:53 -0800

    initial commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+/**
+   * first - function executed before main
+    * Return: no return.
+     */
+
+
+
+void __attribute__ ((constructor)) first()
+
+{
+
+		printf("You're beat! and yet, you must allow");
+
+			printf(",\nI bore my house upon my back!\n");
+
+}
